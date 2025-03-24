7870d9a25a2bc34869e5168065f41e99122f41e8
kunwei <kunwei@kooboo.cn>
2025-03-24 14:38:05 +0800

    fix: make override works

--- utils/override.ts
+++ utils/override.ts
@@ -3,93 +3,83 @@
 
 import type { ComputedGetter, DebuggerOptions } from 'vue'
 
-interface Overridable<T> {
-  __base: T
-  __override?: T
+function isComputedRef<T>(obj: any): obj is ComputedRef<T> {
+  return obj && 'value' in obj
 }
 
-declare function isComputedRef<T>(obj: ComputedRef<T> | unknown): obj is ComputedRef<T>
-
-function createFunctionProxy<T extends Function>(base: T): T & Overridable<T> {
-  let override: T
+function createFunctionProxy<T extends Function>(base: T): T {
+  const override: { value?: T } = {}
   return new Proxy(base, {
     get(target, prop, receiver) {
-      if (prop === '__override') return override
+      if (prop === '__override') return override.value
       if (prop === '__base') return target
       return Reflect.get(target, prop, receiver)
     },
     set(target, prop, value, receiver) {
       if (prop === '__override') {
-        override = value
+        override.value = value
         return true
       }
       return Reflect.set(target, prop, value, receiver)
     },
     apply(target, thisArg, args) {
-      return Reflect.apply(override || target, thisArg, args)
+      return Reflect.apply(override.value || target, thisArg, args)
     },
-  }) as T & Overridable<T>
+  })
 }
 
-function createComputedProxy<T>(base: ComputedRef<T>): ComputedRef<T> & Overridable<ComputedRef<T>> {
-  let override: ComputedRef<T>
+function createComputedProxy<T>(base: ComputedRef<T>): ComputedRef<T> {
+  const override: { value?: ComputedRef<T> } = {}
   return new Proxy(base, {
     get(target, prop, receiver) {
-      if (prop === 'value') return override.value || target.value
-      if (prop === '__override') return override
+      if (prop === 'value') return (override.value || target).value
+      if (prop === '__override') return override.value
       if (prop === '__base') return target
       return Reflect.get(target, prop, receiver)
     },
     set(target, prop, value, receiver) {
       if (prop === '__override') {
-        override = value
+        override.value = value
         return true
       }
       return Reflect.set(target, prop, value, receiver)
     },
-  }) as ComputedRef<T> & Overridable<ComputedRef<T>>
+  })
 }
 
-export function overridable<T extends Function>(func: T): T & Overridable<T>
-export function overridable<T>(computedRef: ComputedRef<T>): ComputedRef<T> & Overridable<ComputedRef<T>>
+export function overridable<T extends Function>(func: T): T
+export function overridable<T>(computedRef: ComputedRef<T>): ComputedRef<T>
 export function overridable<T>(base: T): unknown {
   if (typeof base === 'function') return createFunctionProxy(base)
   if (isComputedRef(base)) return createComputedProxy(base)
   throw new Error('Unsupported type for overridable')
 }
 
-export function ofunc<T extends Function>(func: T): T & Overridable<T> {
+export function ofunc<T extends Function>(func: T): T {
   return overridable(func)
 }
 
-export function ocomputed<T>(getter: ComputedGetter<T>, debugOptions?: DebuggerOptions): ComputedRef<T> & Overridable<ComputedRef<T>> {
+export function ocomputed<T>(getter: ComputedGetter<T>, debugOptions?: DebuggerOptions): ComputedRef<T> {
   return overridable(computed(getter, debugOptions))
 }
 
-export function override<T>(base: Overridable<T>, override: T): void {
-  base.__override = override
+export function override<T>(base: T, override: T): void {
+  (base as any).__override = override
 }
 
 /*
   oextends
 */
-
-type ComposableFunction<T extends object> = (...args: any[]) => T
-
-type ExtensionContext<T extends object> = {
-  target: T
-  base: { [K in keyof T]: T[K] }
+type ExtensionContext<R> = {
+  target: R
+  base: R
 }
 
-export function oextends<
-  T extends object,
-  F extends ComposableFunction<T>,
-  E extends object,
->(
-  baseComposable: F,
-  extend: (ctx: ExtensionContext<T>, ...args: Parameters<F>) => E,
-): (...args: Parameters<F>) => T & E {
-  return (...args: Parameters<F>): T & E => {
+export function oextends<C extends (...args: any[]) => any, E>(
+  baseComposable: C,
+  extend: (ctx: ExtensionContext<ReturnType<C>>, ...args: Parameters<C>) => E,
+): (...args: Parameters<C>) => ReturnType<C> & E {
+  return (...args: Parameters<C>): ReturnType<C> & E => {
     const target = baseComposable(...args)
 
     const base = Object.fromEntries(
@@ -97,11 +87,11 @@ export function oextends<
         key,
         (val as any)?.['__base'] ?? val,
       ]),
-    ) as { [K in keyof T]: T[K] }
+    ) as ReturnType<C>
 
     return Object.assign(
-      target as T,
+      target,
       extend({ target, base }, ...args),
-    ) as T & E
+    )
   }
 }
